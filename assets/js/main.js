95b6d0a8fcf5eda506adaacb741765df6fa8df9b
Savoy <mateus-santos-2011@live.com>
2024-12-11 13:20:05 -0300

    structure done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,43 @@
-const menuButton = document.querySelector('.menu-hamburguer'); // Botão do menu
-const containerNav = document.querySelector('.container-nav'); // Menu lateral
-const backButton = document.querySelector('.back'); // Botão de voltar
+const menuButton = document.querySelector('.menu-hamburguer'); 
+const containerNav = document.querySelector('.container-nav'); 
+const backButton = document.querySelector('.back'); 
 const bgNav = document.querySelector('.bg-nav');
 
-// Mostrar o menu ao clicar no botão hambúrguer
 menuButton.addEventListener('click', () => {
-  containerNav.classList.add('active'); // Exibe o menu
+  containerNav.classList.add('active');
   bgNav.classList.add('bg-nav__active');
 });
 
-// Ocultar o menu ao clicar no botão voltar
+
 backButton.addEventListener('click', () => {
-  containerNav.classList.remove('active'); // Esconde o menu
+  containerNav.classList.remove('active');
   bgNav.classList.remove('bg-nav__active');
 });
+
+function validateResult() {
+
+  const correctResult = 427 + 628;
+
+
+  const userResult = parseInt(document.getElementById('userResult').value, 10);
+
+  const feedback = document.getElementById('feedback');
+
+  const feedbackMobile = document.getElementById('feedback-mobile');
+
+  if (userResult === correctResult) {
+    feedback.textContent = 'Resposta correta!';
+    feedback.style.color = 'green'; 
+  } else {
+    feedback.textContent = 'Resposta incorreta. Tente novamente!';
+    feedback.style.color = 'red';
+  }
+
+  if (userResult === correctResult) {
+    feedbackMobile.textContent = 'Resposta correta!';
+    feedbackMobile.style.color = 'green'; 
+  } else {
+    feedbackMobile.textContent = 'Resposta incorreta. Tente novamente!';
+    feedbackMobile.style.color = 'red'; 
+  }
+}
